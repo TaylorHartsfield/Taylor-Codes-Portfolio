4e8403e01f8d4968877606af5fcefaecbf0da598
Taylor Hartsfield <taylormhartsfield@gmail.com>
2022-10-23 16:02:25 -0600

    Download PDF Resume working

--- src/components/resume/resume.jsx
+++ src/components/resume/resume.jsx
@@ -3,8 +3,27 @@ import TaylorResume from "../assets/TaylorResume.pdf";
 
 export default function Resume(){
 
+    function handleOnClick(){
+        fetch(TaylorResume).then(respose => {
+            respose.blob().then(blob => {
+                const fileURL = window.URL.createObjectURL(blob);
+                let alink = document.createElement('a');
+                alink.href = fileURL;
+                alink.download = TaylorResume;
+                alink.click();
+            })
+        })
+    }
+
+    function Download(){
+        return (
+            <button onClick={handleOnClick}>Download Resume PDF</button>
+        )
+    }
+
     return(
         <Document file = {TaylorResume}>
+            <Download />
             <Page pageNumber={1}/>
         </Document>
     )
