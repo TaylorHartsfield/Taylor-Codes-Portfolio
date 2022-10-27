8b3d4fdd2d64b2eed9020e0217d02dc9696c07d1
Taylor Hartsfield <taylormhartsfield@gmail.com>
2022-10-26 23:54:02 -0600

    Under construction pages

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -5,6 +5,9 @@ export default function About(){
       <div className="content-container">
         <Container className="about">
             <h3 className="text-center mx-auto">About Me</h3>
+            <p style={{textAlign:"center"}}>Under construction! Check out my <a href="https://github.com/TaylorHartsfield">GITHUB</a> for the latest projects.
+                    <br/>Use the contact page to get in touch!
+                    <br/> Want to know a little about my past life?<br/> <a href="https://www.youtube.com/watch?v=vy4U4TL-GTw">Watch me sing and dance!</a></p>
         </Container>
         </div>
        
