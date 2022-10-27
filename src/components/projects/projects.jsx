4f9421c4c82be4a959c509ea67079239392895c0
Taylor Hartsfield <taylormhartsfield@gmail.com>
2022-10-27 08:53:17 -0600

    add traffic light link

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -6,8 +6,9 @@ export default function Projects(){
                   <h3 className="text-center mx-auto">View My Projects</h3>
                   <p style={{textAlign:"center"}}>Under construction! Check out my <a href="https://github.com/TaylorHartsfield">GITHUB</a> for the latest projects.<br/>
                     PS: This entire site is a project of mine. Have a look around!
-                    <br/>PPS: And of course a <a href="https://taylorhartsfield.github.io/ReactWeatherApp/">🌦</a> app! Because every new dev needs one!
-                    <br/>PPPS: Use the contact page to get in touch!</p>
+                    <br>PPS: Here is a fun little <a href="https://taylorhartsfield.github.io/trafficlight/">🚦</a> I made</br>
+                    <br/>PPPS: And of course a <a href="https://taylorhartsfield.github.io/ReactWeatherApp/">🌦</a> app! Because every new dev needs one!
+                    <br/><bold>PPPPS: Use the contact page to get in touch!</bold></p>
               </Container>
               </div>
              
