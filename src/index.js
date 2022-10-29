d54808e8eecf1cff70ce7d888b5dae50d1fa330d
Taylor Hartsfield <taylormhartsfield@gmail.com>
2022-10-29 17:43:15 -0600

    switch to hash router

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from './App';
-import {BrowserRouter} from "react-router-dom" 
+import {HashRouter} from "react-router-dom" 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
