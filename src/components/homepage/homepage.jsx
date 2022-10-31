1b0c077e2acdd9a47bd54df3e2125ce11f4a01ab
Taylor Hartsfield <taylormhartsfield@gmail.com>
2022-10-30 19:22:30 -0600

    homepage restyling

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -2,48 +2,69 @@ import Name from "./name";
 import {Container, Row, Col} from 'react-bootstrap';
 import Headshot from '../assets/headshot.jpeg';
 import LeanrningCounter from "./learningCounter";
+import Github from "../assets/github.png";
+import LinekdIn from "../assets/linkedin.png";
+import YouTube from "../assets/youtube.png";
+import Instagram from "../assets/instagram.png";
 
 export default function Homepage(){
 
+
+    function handleGithub (){
+        window.open('https://github.com/taylorhartsfield', "_blank")
+    }
+    function handleLinkedIn(){
+        window.open('https://linkedin.com/in/taylormhartsfield', "_blank")
+    }
+    
+    function handleYouTube() {
+        window.open('https://www.youtube.com/watch?v=T5ZozcJGobc&list=PL4n4VHJ7Pj_AhvDLaUozrTFcZfdtZRJp8', '_blank')
+    }
+
+    function handleInstagram(){
+        window.open('https://www.instagram.com/tmwhitlow/', '_blank')
+    }
+
     return (
        <div className="homepage-container">
-            <Container style={{
-                margin: "0",
-                alignContent: "center",
-                justifyContent: "center",
-                paddingTop: "15%", 
-            }}>
+            <Container className="homepage">
                 <Row>
-                    <Col xs={{span: 12, order:2 }} 
+                    <Col xs={{span: 12}} 
                         sm={{span: 12, order:2 }} 
                         md={{span: 12, order:2 }} 
-                        lg={{span: 6, order: 1 }} 
-                        xl={{span: 6, order: 1 }}
-                        >
-                            <img src={Headshot} alt="headshot" 
-                            style={{
-                                borderRadius: "50%",
-                                width: "auto",
-                                maxWidth: "400px",
-                                height: "auto",
-                                maxHeight: "400px",
-                                border: "solid",
-                                borderColor: "#a786df",
-                                borderWidth: "10px",
-                                dropShadow: "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px",
-                            }}/>
+                        l={{span: 6, order: 1 }} 
+                        xl={{span: 6, order: 1 }}>
+                        <img className="headshot" src={Headshot} alt="headshot"/>
                     </Col>
                     <Col xs={{span: 12, order:1}} 
                          sm={{span: 12, order:1 }} 
                          md={{span: 12, order:1 }} 
-                         lg={{span: 6, order:2 }} 
-                         xl={{span: 6, order:2 }}>
+                         l={{span: 6, order:2 }} 
+                         xl={{span: 6, order:2 }}
+                        >
                         <Name />
                         <h4 style={{
+                            textAlign: "center",
                             fontSize: "4vh",
-                            paddingBottom: "2em"}}>
-                        Full Stack Developer</h4>
-                        <p className="text-center text-sm-start" style={{display:"inline"}}>It's been <bold style={{color:"#078080"}}>{<LeanrningCounter />} days</bold> since I wrote my first 'Hello World'!</p>
+                            color: "#172c66"}}>
+                        Software Engineer
+                        </h4>
+                        <div className="socials">
+                        <p
+                        style={{textAlign: "center",
+                                display:"inline",
+                                fontSize: '1.5em'}}>
+                        It's been <strong style={{color:"#078080"}}>{<LeanrningCounter />} days </strong>
+                        since I wrote my first 'Hello World'!</p>
+                        </div>
+                        <div className="socials">
+                            <img className="footeritem" src={Github} onClick={handleGithub}/>
+                            <img className="footeritem" src={LinekdIn} onClick={handleLinkedIn}/>
+                            <img className="footeritem" src={Instagram} onClick={handleInstagram}/>
+                            <img className="footeritem" src={YouTube} onClick={handleYouTube}/>
+                        </div>
+
+                      
                     </Col>
                     </Row>
             
--- src/components/homepage/name.jsx
+++ src/components/homepage/name.jsx
@@ -1,9 +1,12 @@
 export default function Name(){
 
     return (
-        <div style={{paddingBottom: "1em"}}>
-            <h1>Taylor</h1>
-            <h1>Hartsfield</h1>
+        <div style={{paddingBottom: "3vh",
+                textAlign:"center"}}>
+            <h1
+            style={{display: "inline",fontSize: "6vh"}}
+            >Taylor Hartsfield</h1>
+            
         </div>
     )
 }
